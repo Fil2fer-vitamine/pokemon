fa700d9ad684184e5093a5ecc851c4c677cbdfe9
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-26 16:45:17 +0100

    useState mis en place - affichage tableau

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, { FunctionComponent, useState } from 'react';
+import Pokemon from './models/pokemon';
+import POKEMONS from './models/mock-pokemon';
 
 const App: FunctionComponent = () => {
   // la valeur name est initialisée avec la valeur "React"
-  const [name, setName] = useState<string>('React');
+  const [pokemons, setPokemons] = useState<Pokemon[]>(POKEMONS);
   // Cette fonction retourne une paire de deux éléments :
   // l'état actuel ('name')
   // et une fonction pour modifier la valeur de cet état ('setName()').
-  return <h1>Bonjour {name} !</h1>;
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+    </div>
+  );
 };
 
 export default App;
@@ -24,7 +31,12 @@ export default App;
  * En ce qui concerne le typage imposé par Typescript, le useState est
  * typé en 'string' au niveau de <>
  */
-// 
+//
 
-
-
+/**
+ * Définir une variable d'état dans le composant App.tsx
+ * !!! Correction : importation de mock-pokemon.ts et pokemon.ts
+ * Initialiser cet état avec la liste de pokémon.
+ * Typer cet état afin qu'il ait un tableau de pokemons.
+ * Afficher le nombre de pokemons présents dans l'état du composant.
+ */
