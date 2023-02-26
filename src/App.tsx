8b7559094d76ae2c7efefefef0bb708a7893d34f
Fil2fer-vitamine <pire.de.la.rate@gmail.com>
2023-02-26 15:58:33 +0100

    Mise en route de l'application Reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FunctionComponent = () => {
+  // la valeur name est initialisée avec la valeur "React"
+  const [name, setName] = useState<string>('React');
+  // Cette fonction retourne une paire de deux éléments :
+  // l'état actuel ('name')
+  // et une fonction pour modifier la valeur de cet état ('setName()').
+  return <h1>Bonjour {name} !</h1>;
+};
 
 export default App;
+/**
+ *      DESTRUCTURING
+ *
+ * const App: FunctionComponent = () => {}
+ *
+ * est la même chose que :
+ * let nameStateVariable = useState('React');
+ * let name = nameStateVariable[0];
+ * let setName = nameStateVariable[1];
+ */
+/**
+ * En ce qui concerne le typage imposé par Typescript, le useState est
+ * typé en 'string' au niveau de <>
+ */
+// 
+
+
+
